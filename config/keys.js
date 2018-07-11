f824bafd3e00c667e01b922dc69592624781214d
Sailesh Kasaju <leosailesh@gmail.com>
2018-07-11 08:50:05 +0545

    db configuration setup done

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://root:test1234@ds233541.mlab.com:33541/dbalpha"
+};
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true },
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello'));
 
 const port = process.env.port || 5000;
 
