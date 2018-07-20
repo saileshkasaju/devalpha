d173b7eeef7b3a6a982bb20fd80fa48f38c9c8c0
saileshkasaju <leosailesh@gmail.com>
2018-07-20 09:22:48 +0545

    profile update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile Model
 const Profile = require('../../models/Profile');
 // Load User Model
@@ -35,10 +38,16 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 
 /**
  * @route POST api/profile
- * @description Create user Profile
+ * @description Create or edit user profile
  * @access Private
  */
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateProfileInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    // Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
   const {
     user: { id },
     body: {
