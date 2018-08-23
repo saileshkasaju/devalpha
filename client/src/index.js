39fb9e01711313a0a41a05f24ae1c33374e151e0
Sailesh Kasaju <leosailesh@gmail.com>
2018-08-23 10:57:08 +0545

    deploy setup client api for heroku

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// axios.defaults.headers.common = { 'X-Requested-With': 'XMLHttpRequest' };
-axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/' : 'https://devalpha.openode.io/';
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/' : 'https://devalpha.openode.io/';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
