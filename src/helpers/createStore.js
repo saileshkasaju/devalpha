4384cfe7126cb8644fae6cfe35199bbb6910c88e
Sailesh Kasaju <leosailesh@gmail.com>
2018-08-31 22:07:18 +0545

    clean code and ssr setup

---
+++ src/helpers/createStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import reducers from '../client/reducers';
+
+export default req => {
+  const axiosInstance = axios.create({
+    baseURL: 'https://devalpha.openode.io/api',
+    headers: { cookie: req.get('cookie') || '' },
+  });
+
+  const store = createStore(reducers, {}, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
+
+  return store;
+};
